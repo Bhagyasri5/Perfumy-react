c799570a7040e551ab38c0b859afbe1e0bc50f7a
Bhagyasri5 <bhagyamano2005@gmail.com>
2024-05-30 13:48:12 +0530

    update project

--- src/index.js
+++ src/index.js
@@ -1,17 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import "./style.css";
+import App from "./app.js";
+
+//components
+//loading images
+// import one from "./assets/images/one.jpg"
+// import two from "./assets/images/two.jpg"
+// import three from "./assets/images/three.jpg"
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+//components
+//navbar
+//searchbar
+//products
+//about
+//footer
+
+root.render(<App></App>);
+
+
+
